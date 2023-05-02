6ab6ae035faa105a5cfeaf528ba9600e90adea21
eugeniemultrier1 <eugenie.multrier@polytechnique.edu>
2023-05-02 06:32:46 -0700

    correction of hierarchy

--- scvi/external/schanvi/_utils.py
+++ scvi/external/schanvi/_utils.py
@@ -27,19 +27,16 @@ from scvi.dataloaders._data_splitting import validate_data_split
 """
 Dictionary for HLCA hierarchy.
 """
-#hierarchy extracted by hand from the dataset and the cell ontology
-#Basal resting? Migratory DCs? Pericytes ? Neuroendocrine ? Ionocyte?
-
+#hierarchy extracted by hand from the dataset  
 hierarchy = {
-    "Airway epithelium": ['Suprabasal', "EC general capillary", "EC aerocyte capillary",  'Goblet (nasal)', 'Ionocyte', 'Club (nasal)'],
-    "Alveolar epithelium": ['Multiciliated (non-nasal)', "Club (non-nasal)", "AT2", "EC venous pulmonary", "Transitional Club-AT2", "AT1", 'AT2 proliferating', 'Neuroendocrine', 'Ionocyte'],
-    'Blood vessels' : ['EC arterial', 'EC venous systemic', 'Pericytes'],
-    'Fibroblast lineage' :['Alveolar fibroblasts', 'Myofibroblasts', 'Adventitial fibroblasts', 'Fibromyocytes', 'Peribronchial fibroblasts'],
-    'Lymphatic EC' :['Lymphatic EC mature', 'Basal resting'],
-    'Lymphoid' :['CD8 T cells', 'CD4 T cells', 'NK cells', 'Plasma cells', 'B cells','DC2','DC1','Plasmacytoid DCs', 'T cells proliferating', 'Migratory DCs'],
-    'Fibroblast lineage' :['Alveolar fibroblasts', 'Myofibroblasts', 'Adventitial fibroblasts', 'Fibromyocytes', 'Peribronchial fibroblasts'],
-    'Myeloid' :['Non-classical monocytes','Mast cells','Classical monocytes', 'Interstitial Mφ perivascular', 'Alveolar macrophages', 'Monocyte-derived Mφ', 'Alveolar Mφ proliferating'],
-    'Smooth muscle' :['Smooth muscle', 'SM activated stress response'] 
+    "Airway epithelium": ['Suprabasal',  'Goblet (nasal)', 'Ionocyte', 'Club (nasal)', 'Basal resting', 'Neuroendocrine', 'Multiciliated (non-nasal)',"Club (non-nasal)", "Transitional Club-AT2"],
+    "Alveolar epithelium": [ "AT2", "AT1", 'AT2 proliferating', 'Ionocyte'],
+    'Blood vessels' : ['EC arterial', 'EC venous systemic', "EC general capillary", "EC aerocyte capillary", "EC venous pulmonary"],
+    'Fibroblast lineage' :['Alveolar fibroblasts', 'Myofibroblasts', 'Adventitial fibroblasts', 'Peribronchial fibroblasts', 'Pericytes'],
+    'Lymphatic EC' :['Lymphatic EC mature'],
+    'Lymphoid' :['CD8 T cells', 'CD4 T cells', 'NK cells', 'Plasma cells', 'B cells', 'T cells proliferating'],
+    'Myeloid' :['Non-classical monocytes','Mast cells','Classical monocytes', 'Interstitial Mφ perivascular', 'Alveolar macrophages', 'Monocyte-derived Mφ', 'Alveolar Mφ proliferating', 'Migratory DCs', 'DC2', 'DC1', 'Plasmacytoid DCs'],
+    'Smooth muscle' :['Smooth muscle', 'SM activated stress response', 'Fibromyocytes'] 
 }
 
 num_classes = [8, 44]
